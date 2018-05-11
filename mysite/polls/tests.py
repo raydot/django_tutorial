9b580119ab71584b9bc8502986d3956998e44c26
Dave Kanter <moatgoat@hotmail.com>
2018-05-11 15:39:32 -0700

    Finished the unit testing lesson.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -40,6 +40,12 @@ def create_question(question_text, days):
 	""" 
 	Create a question with the given 'question text' and published the
 	given number of 'days' offset to now (negative for past, positive for future)
+	This function is used in the tests that follow.
+
+	"In effect, we are using the tests to tell a story of admin 
+	input and user experience on the site, and checking that at 
+	every state and for every new change in the state of the system, 
+	the expected results are published."
 	"""
 	time = timezone.now() + datetime.timedelta(days=days)
 	return Question.objects.create(question_text=question_text, pub_date=time)
@@ -57,4 +63,31 @@ self.assertQuerysetEqual(response.context['latest_question_list'], [])
 		"""
 		Questions with a pub_date in the past are displayed on the index page
 		"""
-		
+		create_question(question_text="Past question.", days=-30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertQuerysetEqual(
+			['latest_question_list'], ['<Question: Past question.>']
+		)
+
+	def test_future_question(self):
+		"""
+		Questions with a pub_date in the future aren't displayed
+		on the index page.
+		"""
+		create_question(question_text="Future question.", days=30)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response,"No polls are available.")
+
+self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+def test_two_past_questions(self):
+	"""
+	The questions index page may display multiple questions.
+	"""
+	create_question(question_text="Past question 1.", days=-30)
+	create_question(question_text="Past question 2.", days=-5)
+	response = self.client.get(reverse('polls:index'))
+	self.assertQuerysetEqual(
+		response.content['latest_question_list'], 
+		['<Question: Past question 2.>', '<Question: Past question 1.>']
+	)
